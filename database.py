36b7049824d32cc8df1df9ae0b8e49345a41fa35
S7b0t4 <toni.paun.00@mail.ru>
2025-07-23 20:47:14 +0500

    feat: first commit

---
+++ database.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Column, Integer, String, BigInteger, Boolean, select
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import asyncio
+
+DATABASE_URL = 'postgresql+asyncpg://user:password@77.91.70.186:6543/fizegebot'
+engine = create_async_engine(DATABASE_URL, future=True, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tg_id = Column(BigInteger, primary_key=True, default=0)
+    username = Column(String)
+    total_tasks = Column(Integer, default=0)
+    correct = Column(Integer, default=0)
+    wrong = Column(Integer, default=0)
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String)
+    is_right_answer = Column(Boolean, default=True)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_user(session: AsyncSession, tg_id: BigInteger, username: str = None) -> User:
+    print(tg_id, username)
+    result = await session.execute(
+        select(User).where(User.tg_id == tg_id)
+    )
+    user = result.scalar_one_or_none()
+
+    if not user:
+        user = User(tg_id=tg_id, username=username)
+        session.add(user)
+        await session.commit()
+    elif username and user.username != username:
+        user.username = username
+        await session.commit()
+    print(user.id, user.username, user.total_tasks)
+    return user
+
+async def get_task(session: AsyncSession, task_text: str, is_right: Boolean) -> Task:
+    task = Task(description=task_text, is_right_answer=is_right)
+    session.add(task)
+    await session.commit()
+    return task
+
+
+async def main():
+    await init_db()
+
+async def createUser(tg_id, username):
+    async with async_session() as session:
+        user = await get_user(session, tg_id=tg_id, username=username)
+
+async def createTask(task_text, is_right):
+    async with async_session() as session:
+        task = await get_task(session, task_text=task_text, is_right=is_right)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ main.py
@@ -0,0 +1,105 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from aiogram.filters import Command
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from aiogram import F
+from database import createUser, createTask
+
+api_token = '7358477783:AAFqhM5DZUWF18keUNoFC0EV6I5PZrlxD50'
+bot = Bot(token=api_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(Command('start'))
+async def cmd_start(message:Message):
+    user = message.from_user
+    await createUser(user.id, user.username)
+    keyboard1 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Практика по заданиям', callback_data='practice')],
+        [InlineKeyboardButton(text='Мой профиль', callback_data='profile')],
+        [InlineKeyboardButton(text='Добавить задачу', callback_data='add_task')]
+    ])
+    await message.answer('Привет! Это бот для практики по заданиям из ЕГЭ по физике.', reply_markup=keyboard1)
+
+# Фильтры для кнопок
+
+@dp.callback_query(F.data == 'practice')
+async def practice_handler(callback: CallbackQuery):
+    keyboard2 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Задание номер 18', callback_data='task18')],
+        [InlineKeyboardButton(text='Назад', callback_data='start')]
+    ])
+    await callback.message.answer('Выбери раздел:', reply_markup=keyboard2)
+
+@dp.callback_query(F.data == 'profile')
+async def profile_handler(callback: CallbackQuery):
+    keyboard3 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Назад', callback_data='start')]
+    ])
+    await callback.message.answer('Профиль пользователя:\nПройдено заданий:\nОтвечено правильно:'
+                                  '\nОтвечено неправильно:\nМесто в рейтинге:', reply_markup=keyboard3)
+
+#Добавить задачу
+
+class AddTaskState(StatesGroup):
+    waiting_for_task_text = State()
+    waiting_for_answer_type = State()
+
+@dp.callback_query(F.data == 'add_task')
+async def add_task_handler(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(AddTaskState.waiting_for_task_text)
+    keyboard2 = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text='Назад', callback_data='start')]
+    ])
+    await callback.message.answer('Введите описание:', reply_markup=keyboard2)
+
+@dp.message(AddTaskState.waiting_for_task_text)
+async def get_task_text(message: Message, state: FSMContext):
+    await state.update_data(task_text=message.text)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="✅ Правильный", callback_data="answer_right")],
+        [InlineKeyboardButton(text="❌ Неправильный", callback_data="answer_wrong")],
+        [InlineKeyboardButton(text='Назад', callback_data='start')]
+    ])
+
+    await message.answer("Это правильный ответ или неправильный?", reply_markup=keyboard)
+    await state.set_state(AddTaskState.waiting_for_answer_type)
+
+@dp.callback_query(AddTaskState.waiting_for_answer_type)
+async def get_answer_type(callback: CallbackQuery, state: FSMContext):
+    if callback.data == 'start':
+        await state.clear()
+        await callback.message.answer("Отменено. Возвращаюсь в главное меню.")
+        await cmd_start(callback.message)
+        await callback.answer()
+        return
+
+    data = await state.get_data()
+    task_text = data['task_text']
+
+    is_right = callback.data == "answer_right"
+
+    # Здесь можно сохранить в БД, например:
+    await createTask(task_text, is_right)
+
+    await callback.message.answer("Задача добавлена ✅")
+    await state.clear()
+
+
+#Назад
+
+@dp.callback_query(F.data == 'start')
+async def practice_handler(callback: CallbackQuery):
+    await cmd_start(callback.message)
+    await callback.answer()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
